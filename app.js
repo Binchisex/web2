c13582dcc8f9678631c49ff250e16b0e56f7c8cb
Binchisex <binchisas@gmail.com>
2025-05-23 20:13:38 -0600

    fix: agregado build de frontend a app.js

--- app.js
+++ app.js
@@ -1,31 +1,38 @@
-//configuracion de express
-
+// configuración de express
 import express from "express";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 
-import UsuarioRoutes from "./Rutas/usuario.rutas.js"
-import RecetaRouters from "./Rutas/receta.rutas.js"
-import PasoRouter from "./Rutas/paso.rutas.js"
-import CategoriasRouter from "./Rutas/categoria.rutas.js"
-import ComentarioRouter from "./Rutas/comentario.rutas.js"
-import TableroRouter from "./Rutas/tablero.rutas.js"
-import FavoritosRouter from "./Rutas/favoritos.rutas.js"
-
+import UsuarioRoutes from "./Rutas/usuario.rutas.js";
+import RecetaRouters from "./Rutas/receta.rutas.js";
+import PasoRouter from "./Rutas/paso.rutas.js";
+import CategoriasRouter from "./Rutas/categoria.rutas.js";
+import ComentarioRouter from "./Rutas/comentario.rutas.js";
+import TableroRouter from "./Rutas/tablero.rutas.js";
+import FavoritosRouter from "./Rutas/favoritos.rutas.js";
 
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/usuarios",UsuarioRoutes);
-app.use("/recetas",RecetaRouters);
-app.use("/pasos",PasoRouter);
-app.use("/categorias",CategoriasRouter);
-app.use("/comentarios",ComentarioRouter);
-app.use("/tableros",TableroRouter);
-app.use("/favoritos",FavoritosRouter);
+// API routes
+app.use("/usuarios", UsuarioRoutes);
+app.use("/recetas", RecetaRouters);
+app.use("/pasos", PasoRouter);
+app.use("/categorias", CategoriasRouter);
+app.use("/comentarios", ComentarioRouter);
+app.use("/tableros", TableroRouter);
+app.use("/favoritos", FavoritosRouter);
 
+// Servir el frontend
+app.use(express.static(path.join(__dirname, "frontend", "build")));
 
-
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+});
 
 export default app;
